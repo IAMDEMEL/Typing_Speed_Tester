b3879b54f9c1b66ad0121c6fd775fac4975edb47
Demel <iamdemel@outlook.com>
2023-11-30 18:54:26 -0800

    Start Project
    Created a Paragraph Generator with  a Brain Class for functionality
    
    Created a random paragraph generator
    
    Created a normal paragraph generator
    
     - Every time the user chooses a normal paragraph it will add a new paragraph to a "Normal Paragraphs.csv"
    
    Create a list of paragraphs the user can user without the need for the internet

---
+++ main.py
@@ -0,0 +1,5 @@
+from sentence_generator import Brain
+
+generator = Brain()
+# print(generator.generate_random_paragraph())
+print(generator.generator_normal_paragraph())
---
+++ sentence_generator.py
@@ -0,0 +1,102 @@
+from requests.exceptions import ConnectionError
+from bs4 import BeautifulSoup
+from _csv import writer
+import pandas as pd
+import requests
+import random
+import string
+
+
+def grab_paragraph_of_the_web(amount_to_grab=3):
+    readable_paragraph = ''
+    for i in range(amount_to_grab):
+        website = 'https://randomword.com/paragraph'
+        response = requests.get(website)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        paragraph = soup.find_all(id="random_word_definition")[0]
+        readable_paragraph += paragraph.text
+        if amount_to_grab > 1 and i < amount_to_grab - 1:
+            readable_paragraph += '\n\n'
+    return readable_paragraph
+
+
+class Brain:
+    def __init__(self):
+        self.countdown = 0
+        self.characters_in_word = 0
+        self.words_in_sentence = 0
+        self.current_sentence = ''
+        self.sentences_in_paragraph = 0
+        self.paragraphs = None
+        self.readable_paragraphs = ''
+
+    def generate_random_paragraph(self):
+        self.paragraphs = random.randint(1, 4)
+
+        for paragraph in range(self.paragraphs):
+            self.sentences_in_paragraph = random.randint(4, 8)
+
+            for sentence in range(self.sentences_in_paragraph):
+                short_sentence_length = 10
+                self.words_in_sentence = random.randint(3, 19)
+                if self.words_in_sentence >= short_sentence_length:
+                    self.countdown = random.randint(3, self.words_in_sentence - 3)
+
+                for word in range(self.words_in_sentence):
+                    self.characters_in_word = random.randint(1, 12)
+
+                    if self.words_in_sentence >= short_sentence_length:
+                        if self.countdown > 0:
+                            self.countdown -= 1
+                            if self.countdown == 0:
+                                structuring_list = [',', ';']
+                                self.current_sentence += random.choice(structuring_list)
+                    self.current_sentence += ' '
+
+                    while self.characters_in_word > 1:
+                        self.characters_in_word -= 1
+                        self.current_sentence += random.choice(string.ascii_lowercase)
+
+                while self.current_sentence[0] == ' ':
+                    self.current_sentence = self.current_sentence[1:]
+
+                self.current_sentence += '. '  # Todo 1. '\n' is temporary and should be wrapped in the GUI.
+                self.readable_paragraphs += self.current_sentence.capitalize()
+                self.current_sentence = ''
+
+            self.readable_paragraphs += '\n\n'
+
+        return self.readable_paragraphs
+
+    def generator_normal_paragraph(self):
+        try:
+            file = open("Normal Paragraphs.csv", "r")
+            file.close()
+            try:
+                with open("Normal Paragraphs.csv", "a") as file:
+                    self.paragraphs = [grab_paragraph_of_the_web(random.randint(1, 5))]
+                    writer_object = writer(file)
+                    writer_object.writerow(self.paragraphs)
+                    file.close()
+                return self.paragraphs[0]
+            except ConnectionError:
+                df = pd.read_csv('Normal Paragraphs.csv')
+                self.paragraphs = df['Paragraph'].to_list()
+
+                if len(self.paragraphs) >= 5:
+                    return random.choice(self.paragraphs)
+                else:
+                    df = pd.read_csv('No Internet Paragraphs.csv')
+                    self.paragraphs = df['Paragraph'].to_list()
+                    return random.choice(self.paragraphs)
+
+        except FileNotFoundError:
+            try:
+                self.paragraphs = grab_paragraph_of_the_web(random.randint(1, 5))
+                df = pd.DataFrame({'Paragraph': [self.paragraphs]}, index=[0])
+                df.to_csv('Normal Paragraphs.csv', mode='a', index=False)
+                return self.paragraphs
+            except ConnectionError:
+                df = pd.read_csv('No Internet Paragraphs.csv')
+                self.paragraphs = df['Paragraph'].to_list()
+                return random.choice(self.paragraphs)
